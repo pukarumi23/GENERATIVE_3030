cb94188360d9b984072c30e17679101457b0ebcc
pukarumi23 <edwinsantacruzh@gmail.com>
2025-08-10 23:07:40 -0500

    🔶INDEPENDIENTE_BOT🔶

--- plugins/nsfw-pussy.js
+++ plugins/nsfw-pussy.js
@@ -1,5 +1,5 @@
 //Codígo creado por Destroy wa.me/584120346669
-
+/*
 import fs from 'fs';
 import path from 'path';
 
@@ -53,3 +53,4 @@ handler.command = ['lickpussy','coño'];
 handler.group = true;
 
 export default handler;
+*/
