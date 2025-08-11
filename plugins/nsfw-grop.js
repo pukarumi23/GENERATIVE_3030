ee8b9978d9e2b44874dcf864da2ea5e6e8633b70
pukarumi23 <edwinsantacruzh@gmail.com>
2025-08-10 23:06:03 -0500

    🔶INDEPENDIENTE_BOT🔶

--- plugins/nsfw-grop.js
+++ plugins/nsfw-grop.js
@@ -1,5 +1,5 @@
 //Codígo creado por Destroy wa.me/584120346669
-
+/*
 import fs from 'fs';
 import path from 'path';
 
@@ -51,3 +51,4 @@ handler.command = ['grop', 'grope', 'manosear'];
 handler.group = true;
 
 export default handler;
+*/
