7f58d59202a608477b99f419e9d508700737723b
pukarumi23 <edwinsantacruzh@gmail.com>
2025-08-17 23:02:28 -0500

    🔶INDEPENDIENTE_BOT🔶

--- plugins/ai-gemini.js
+++ plugins/ai-gemini.js
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch'
 var handler = async (m, { text,  usedPrefix, command }) => {
-if (!text) return conn.reply(m.chat, `💙 Ingresa una petición para que Gemini Virtual responda con la sabiduría del concierto cibernético de Miku 🎵`, m, rcanal)
+if (!text) return conn.reply(m.chat, `🔶 Ingresa una petición para que Gemini responda.`, m, rcanal)
 try {
 await m.react('🎤')
 conn.sendPresenceUpdate('composing', m.chat)
@@ -8,8 +8,8 @@ var apii = await fetch(`https://apis-starlights-team.koyeb.app/starlight/gemini?
 var res = await apii.json()
 await m.reply(res.result)
 } catch {
-await m.react('💙')
-await conn.reply(m.chat, `💙 ¡Gomen! Gemini Virtual está ensayando en el mundo cibernético y no puede responder en este momento 💫`, m, rcanal)
+await m.react('🍀')
+await conn.reply(m.chat, `🔶 Gemini no puede responder en este momento 💫`, m, rcanal)
 }}
 handler.command = ['gemini']
 handler.help = ['gemini']
