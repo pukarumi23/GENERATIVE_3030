5400bba47111903cc251ed7b31714f5e41b7aab8
pukarumi23 <edwinsantacruzh@gmail.com>
2025-08-17 22:59:28 -0500

    🔶INDEPENDIENTE_BOT🔶

--- plugins/ai-dalle.js
+++ plugins/ai-dalle.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const handler = async (m, { conn, args }) => {
     if (!args[0]) {
-        await conn.reply(m.chat, `💙 Por favor, proporciona una descripción para generar la imagen en el mundo virtual de Miku 🎵`, m, rcanal);
+        await conn.reply(m.chat, `🔶 Por favor, proporciona una descripción para generar la imagen.`, m, rcanal);
         return;
     }
 
@@ -10,14 +10,14 @@ const handler = async (m, { conn, args }) => {
     const apiUrl = `https://eliasar-yt-api.vercel.app/api/ai/text2img?prompt=${prompt}`;
 
     try {
-        conn.reply(m.chat, `💙 ¡Generando tu imagen en el concierto virtual! Por favor espera un momento 🎵`, m)
+        conn.reply(m.chat, `🔶 ¡Generando tu imagen! Por favor espera un momento 🍀`, m)
 
         const response = await axios.get(apiUrl, { responseType: 'arraybuffer' });
 
         await conn.sendMessage(m.chat, { image: Buffer.from(response.data) }, { quoted: m });
     } catch (error) {
         console.error('Error al generar la imagen:', error);
-        await conn.reply(m.chat, `💙 ¡Gomen! No se pudo generar la imagen en el mundo virtual. ¡Inténtalo de nuevo más tarde! 💫`, m, rcanal);
+        await conn.reply(m.chat, `🔶 No se pudo generar la imagen. ¡Inténtalo de nuevo más tarde! 💫`, m, rcanal);
     }
 };
 
