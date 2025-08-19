f1a42ac0618ec1ede7b054e1912481a8642f0715
pukarumi23 <edwinsantacruzh@gmail.com>
2025-08-19 09:33:00 -0500

    🔶INDEPENDIENTE_BOT🔶

--- settings.js
+++ settings.js
@@ -69,7 +69,7 @@ global.correo = 'brauliovh3@gmail.com'
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
-global.catalogo = fs.readFileSync('./src/catalogo.jpg');
+global.catalogo = fs.readFileSync('./src/catalogo.jpeg');
 global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: packname, orderTitle: 'Bang', thumbnail: catalogo, sellerJid: '0@s.whatsapp.net'}}}
 global.ch = {
 ch1: '120363418348675041@newsletter',
