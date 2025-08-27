ad64677d5b9aeb86fa844291352754de13318b6a
pukarumi23 <edwinsantacruzh@gmail.com>
2025-08-27 12:07:12 -0700

    🔶INDEPENDIETE_BOT🔶

--- plugins/rpg-cartera.js
+++ plugins/rpg-cartera.js
@@ -3,7 +3,7 @@ let who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : m
 if (who == conn.user.jid) return error 
 if (!(who in global.db.data.users)) return conn.reply(m.chat, `🔶 ¡Ara ara! Este usuario no está registrado en mi base de datos. ✨`, m , rcanal)
 let user = global.db.data.users[who]
-await m.reply(`${who == m.sender ? `🔶 Tienes *${user.coin} ${moneda} 🎤* en tu cartera. ✨` : `🔶 El usuario @${who.split('@')[0]} tiene *${user.coin} ${moneda} * en su cartera ✨`}. `, null, { mentions: [who] })}
+await m.reply(`${who == m.sender ? `╔═━━━━━━༺༻━━━━━━═╗\n cartera: *${user.coin} ${moneda} * ` : `╔═━━━━━━༺༻━━━━━━═╗\n usuario: @${who.split('@')[0]} \ncartera: *${user.coin} ${moneda} * `}. `, null, { mentions: [who] })}
 
 handler.help = ['wallet']
 handler.tags = ['economy']
