1f7b4b75b040aba215538d82f3ea1209cf378c80
pukarumi23 <edwinsantacruzh@gmail.com>
2025-08-27 07:13:32 -0500

    🔶INDEPENDIENTE_BOT🔶

--- plugins/rpg-bank.js
+++ plugins/rpg-bank.js
@@ -3,19 +3,19 @@ import db from '../lib/database.js'
 let handler = async (m, { conn, usedPrefix }) => {
     let who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : m.sender
     if (who == conn.user.jid) return m.react('🎤')
-    if (!(who in global.db.data.users)) return m.reply(`🎤💙 El usuario no se encuentra en la base de datos del concierto virtual ✨`)
+    if (!(who in global.db.data.users)) return m.reply(`🔶 El usuario no se encuentra en la base de datos del concierto virtual 🌿`)
   
     let user = global.db.data.users[who]
     let total = (user.coin || 0) + (user.bank || 0);
 
-    const texto = `🎤💙 Información Económica del Concierto Virtual ✨
+    const texto = `🔶Información Económica 🔶
 
-🎵 Fanático » *${conn.getName(who)}*   
-💎 Notas Musicales » *${user.coin} ${moneda}*
-🏦 Banco Virtual » *${user.bank} ${moneda}*
-✨ Total » *${total} ${moneda}*
+usuario » *${conn.getName(who)}*   
+Intis » *${user.coin} ${moneda}*
+Banco » *${user.bank} ${moneda}*
+Total » *${total} ${moneda}*
 
-🎤💙 *¡Para proteger tus notas musicales, depósitalas en el banco virtual usando #deposit!* ✨🎵`;
+🔶*¡Para proteger tus Intis, depósitalas en el banco usando #deposit!* 🌿`;
 
     await conn.reply(m.chat, texto, m)
 }
