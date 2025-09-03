532393eb75ec0f88bea9ac6107d50702a649ad56
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2025-09-03 10:00:40 -0500

    FIX

--- plugins/rpg-gacha-inca.js
+++ plugins/rpg-gacha-inca.js
@@ -13,70 +13,70 @@ const personajesIncas = [
         name: "Campesino Quechua",
         rarity: "común",
         probability: 5,  
-        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqMMCKkNvJblbsnAOxtdOfdXZ2L03MIB0DGg&s", //reemplazar con imágenes 
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqMMCKkNvJblbsnAOxtdOfdXZ2L03MIB0DGg&s", 
         description: "Trabajador de los andenes sagrados"
     },
     {
         name: "Tejedora Inca",
         rarity: "común",
         probability: 5,  
-        img: "https://files.catbox.moe/example2.png",
+        img: "https://blog.viajesmachupicchu.travel/wp-content/uploads/2025/05/poblador-paracas-tejiendo-manto-1024x683.jpg",
         description: "Maestra en el arte del cumbi"
     },
     {
         name: "Chasqui Joven",
         rarity: "común",
         probability: 5,  
-        img: "https://files.catbox.moe/example3.png",
+        img: "https://fesofi.es/wp-content/uploads/2021/02/Imagen3.jpg",
         description: "Mensajero de los caminos del Inca"
     },
     {
         name: "Pastor de Llamas",
         rarity: "común",
         probability: 5,
-        img: "https://files.catbox.moe/example4.png",
+        img: "https://i.pinimg.com/474x/a2/c4/f8/a2c4f8aca25ac131429476b4beb2281b.jpg",
         description: "Guardián de los rebaños sagrados"
     },
     {
         name: "Alfarero del Cusco",
         rarity: "común",
         probability: 5,
-        img: "https://files.catbox.moe/example5.png",
+        img: "https://micromuseo.org.pe/wp-content/uploads/1988/09/URTEAGA-OBRAS-ALFARERO-1937-O%CC%81leo-sobre-tela-48.5-x-34.5-cm-Coleccio%CC%81n-particular-Lima-100-DPI-800-PX-w.jpg",
         description: "Artesano de la cerámica imperial"
     },
     {
         name: "Agricultor de Quinua",
         rarity: "común",
         probability: 5,
-        img: "https://files.catbox.moe/example6.png",
+        img: "https://antipode-peru.com/info/-quel-est-l-origine-du-quinoa--1.jpeg",
         description: "Cultivador del grano de oro"
     },
     {
         name: "Pescador del Titicaca",
         rarity: "común",
         probability: 5,
-        img: "https://files.catbox.moe/example7.png",
+        img: "https://www.artmajeur.com/medias/standard/r/o/rolando/artwork/1682701_LAGO_TITICACA.jpg",
         description: "Navegante de las aguas sagradas"
     },
     {
         name: "Minero de Potosí",
         rarity: "común",
         probability: 5,
-        img: "https://files.catbox.moe/example8.png",
+        img: "https://artwork.art-cdn.com/551131/gabrielapgarciayahoocomar-pintura-paisaje-minero-de-potosi.jpg",
         description: "Extractor de la plata del Cerro Rico"
     },
     {
         name: "Cuidadora de Vicuñas",
         rarity: "común",
         probability: 5,
-        img: "https://files.catbox.moe/example9.png",
+        img: "https://img.eldefinido.cl/portadas/1200/2018-10-19-7944KW8005.jpg",
         description: "Protectora de la fibra más fina"
     },
     {
         name: "Constructor Inca",
         rarity: "común",
         probability: 5,
-        img: "https://files.catbox.moe/example10.png",
+        img: "https://trexperienceperu.com/sites/default/files/ayllus-constructores-machu-picchu.webp",
         description: "Maestro de la piedra perfecta"
     },
 
@@ -85,70 +85,70 @@ const personajesIncas = [
         name: "Curaca Regional",
         rarity: "rara",
         probability: 3,
-        img: "https://files.catbox.moe/example11.png",
+        img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhPsfcAxgN9AJdUqAoN3k1VLfobvMz-Pc6dN8Wuuu7WrMmWQ_JXQwJZKK9mz852Y-aCro0iI-DPunz0LW5SelpiapLGTrsS7cdanTaSdT5Hum_plHpZi95QICBgK7eP1vSZ0Zi5wx5pvYo/s400/Imagen112.jpg",
         description: "Gobernador de una región del imperio"
     },
     {
         name: "Quipucamayoc",
         rarity: "rara",
         probability: 3,
-        img: "https://files.catbox.moe/example12.png",
+        img: "https://portal.andina.pe/EDPfotografia3/Thumbnail/2019/11/09/000631869W.jpg",
         description: "Maestro contador de los quipus"
     },
     {
         name: "Amautas Sabio",
         rarity: "rara",
         probability: 3,
-        img: "https://files.catbox.moe/example13.png",
+        img: "https://www.ecured.cu/images/c/ca/Amautas.jpg",
         description: "Educador y filósofo del Cusco"
     },
     {
         name: "Chasqui Veterano",
         rarity: "rara",
         probability: 3,
-        img: "https://files.catbox.moe/example14.png",
+        img: "https://i.pinimg.com/564x/37/b0/f2/37b0f2648de9a869f3d5336dd652293e.jpg",
         description: "Corredor élite del Qhapaq Ñan"
     },
     {
         name: "Sacerdotisa de la Luna",
         rarity: "rara",
         probability: 3,
-        img: "https://files.catbox.moe/example15.png",
+        img: "https://freewalkingtoursperu.com/wp-content/uploads/2018/11/Templo-de-la-Luna-en-Cusco-3.webp",
         description: "Servidora de Mama Quilla"
     },
     {
         name: "Guerrero Antis",
         rarity: "rara",
         probability: 3,
-        img: "https://files.catbox.moe/example16.png",
+        img: "https://i.pinimg.com/474x/a0/99/a8/a099a8af3f76450f63e031d8dcd3ca5c.jpg",
         description: "Defensor de la selva oriental"
     },
     {
         name: "Arquiteco de Sacsayhuamán",
         rarity: "rara",
         probability: 3,
-        img: "https://files.catbox.moe/example17.png",
+        img: "https://www.ktperutravel.com/images/blog/trono-de-inca-sacsayhuaman.jpg",
         description: "Constructor de la fortaleza sagrada"
     },
     {
         name: "Coya Noble",
         rarity: "rara",
         probability: 3,
-        img: "https://files.catbox.moe/example18.png",
+        img: "https://i.pinimg.com/736x/7b/2e/27/7b2e27fb3a9a4716520ecfe005bd49c9.jpg",
         description: "Princesa del linaje real"
     },
     {
         name: "Hatun Runa Distinguido",
         rarity: "rara",
         probability: 3,
-        img: "https://files.catbox.moe/example19.png",
+        img: "https://carpetapedagogica.com/img/articulos/organizacion-economica-inca.jpg",
         description: "Ciudadano honorable del imperio"
     },
     {
         name: "Médico Kallawaya",
         rarity: "rara",
         probability: 3,
-        img: "https://files.catbox.moe/example20.png",
+        img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEh1hoV4XbuBOVIgh953-5DorLhyphenhyphenwAl7b-3l6XBWU7q3Fw30NuniGchgVa-85FFqq_Gtj2O9_U6kUbx55bq9FPLdT3q8TD4fpI-quF-8KFqJHZvs-PvQAifBbdkvJwmCV1y5UcPojQrV09hf/w1200-h630-p-k-no-nu/Jos%25C3%25A9+Manuel+Vald%25C3%25A9s+y+la+historia+de+la+medicina+peruana..jpg",
         description: "Curandero de las montañas sagradas"
     },
 
@@ -157,70 +157,70 @@ const personajesIncas = [
         name: "🌟 Princesa Chuya Occllo 🌟",
         rarity: "épica",
         probability: 1.5,
-        img: "https://files.catbox.moe/example21.png",
+        img: "https://imgmedia.larepublica.pe/640x761/larepublica/migration/images/GWESE6CSFVGIPAMVRJUFGC6VU4.webp",
         description: "Hija del Sol, hermana del Inca"
     },
     {
         name: "⚔️ Rumiñahui el Valiente ⚔️",
         rarity: "épica",
         probability: 1.5,
-        img: "https://files.catbox.moe/example22.png",
+        img: "https://i.pinimg.com/736x/01/5d/2f/015d2f89101ecb2768e4aba078de68b4.jpg",
         description: "General que resistió a los conquistadores"
     },
     {
         name: "🏔️ Capac Yupanqui 🏔️",
         rarity: "épica",
         probability: 1.5,
-        img: "https://files.catbox.moe/example23.png",
+        img: "https://perumillastravel.com/wp-content/uploads/2024/12/Imagen1.jpg",
         description: "Príncipe conquistador de Chinchasuyu"
     },
     {
         name: "🌙 Mama Anahuarque 🌙",
         rarity: "épica",
         probability: 1.5,
-        img: "https://files.catbox.moe/example24.png",
+        img: "https://upload.wikimedia.org/wikipedia/commons/b/bb/Peru_-_Inca_Princess_-_Google_Art_Project.jpg",
         description: "Coya, esposa de Tupac Inca Yupanqui"
     },
     {
         name: "⚡ Quisquis el Estratega ⚡",
         rarity: "épica",
         probability: 1.5,
-        img: "https://files.catbox.moe/example25.png",
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/%22El_General_Quisquis%22_%28s._XIX%29%2C_%C3%B3leo_sobre_lienzo_de_autor_an%C3%B3nimo._MuNa%2C_Quito.jpg/250px-%22El_General_Quisquis%22_%28s._XIX%29%2C_%C3%B3leo_sobre_lienzo_de_autor_an%C3%B3nimo._MuNa%2C_Quito.jpg",
         description: "General de la guerra civil inca"
     },
     {
         name: "🦅 Challcuchima 🦅",
         rarity: "épica",
         probability: 1.5,
-        img: "https://files.catbox.moe/example26.png",
+        img: "https://cloudfront-us-east-1.images.arcpublishing.com/infobae/WT24X5MBHRFWXJMRU6JZUN67GM.png",
         description: "Gran General de Atahualpa"
     },
     {
         name: "💫 Mama Raua Occllo 💫",
         rarity: "épica",
         probability: 1.5,
-        img: "https://files.catbox.moe/example27.png",
+        img: "https://as2.ftcdn.net/jpg/05/74/43/35/1000_F_574433515_ylqNjZTg4fQ6MdRQv6r9FwhqDv5ZVmmW.jpg",
         description: "Coya fundadora del Cusco"
     },
     {
         name: "🌋 Tupac Amaru I 🌋",
         rarity: "épica",
         probability: 1.5,
-        img: "https://files.catbox.moe/example28.png",
+        img: "https://images.ctfassets.net/bv5rnjawitjg/4fXH7eUWK9SqzcPOmQ4WI2/ceed36cb9dbada5927504b7d99c84ae7/foto_01.jpg",
         description: "Último Inca de Vilcabamba"
     },
     {
         name: "⭐ Capac Raymi ⭐",
         rarity: "épica",
         probability: 1.5,
-        img: "https://files.catbox.moe/example29.png",
+        img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhKUYqXjHtl6Xx_QK_H_N2tLE8LRWljVAt3PvosFS2T85DztA8J5qyx7pJIdLOcSfOaMsZSOBclg9H2IVqerTLlyGi5IDzJzjILhmMTnJJPGuX0_-ti3FGOkelEZjS0ionKqjDNApdW_iY/s680/festividad-inca-del-capac-raymi-diciembre.jpg",
         description: "Príncipe del solsticio sagrado"
     },
     {
         name: "🏺 Sinchi Roca 🏺",
         rarity: "épica",
         probability: 1.5,
-        img: "https://files.catbox.moe/example30.png",
+        img: "https://historiadelperu.info/wp-content/uploads/2019/07/inca-sinchi-roca.jpg",
         description: "Segundo Inca del Cusco"
     },
 
@@ -229,70 +229,70 @@ const personajesIncas = [
         name: "☀️ MANCO CAPAC ☀️",
         rarity: "ultra rara",
         probability: 0.4,
-        img: "https://files.catbox.moe/example31.png",
+        img: "https://www.incamachupicchutours.com/wp-content/uploads/2020/12/manco-capac-mama-ocllo.jpg",
         description: "Primer Inca, Hijo del Sol, Fundador del Cusco"
     },
     {
         name: "⚔️ PACHACUTEC INCA YUPANQUI ⚔️",
         rarity: "ultra rara",
         probability: 0.4,
-        img: "https://files.catbox.moe/example32.png",
+        img: "https://www.topalpakatravel.com/wp-content/uploads/inca-pachacutec-911x1024.webp",
         description: "El Transformador del Mundo, Constructor del Imperio"
     },
     {
         name: "🌟 TUPAC INCA YUPANQUI 🌟",
         rarity: "ultra rara",
         probability: 0.4,
-        img: "https://files.catbox.moe/example33.png",
+        img: "https://escuela-gestion.syf.pe/wp-content/uploads/2019/07/Viaje-a-Polinesia-2.jpg",
         description: "Gran Conquistador, Expandió el Tahuantinsuyu"
     },
     {
         name: "👑 HUAYNA CAPAC 👑",
         rarity: "ultra rara",
         probability: 0.4,
-        img: "https://files.catbox.moe/example34.png",
+        img: "https://reinadelaselva.pe/wp-content/uploads/2025/06/huayna.jpg",
         description: "Último Gran Inca, Emperador del Apogeo"
     },
     {
         name: "🦎 LLOQUE YUPANQUI 🦎",
         rarity: "ultra rara",
         probability: 0.4,
-        img: "https://files.catbox.moe/example35.png",
+        img: "https://blog.viajesmachupicchu.travel/wp-content/uploads/2025/05/lloque-yupanqui-portada-1-1170x550.jpg",
         description: "Tercer Inca, El Zurdo Memorable"
     },
     {
         name: "🌸 MAMA OCCLLO 🌸",
         rarity: "ultra rara",
         probability: 0.4,
-        img: "https://files.catbox.moe/example36.png",
+        img: "https://i.pinimg.com/736x/07/9c/de/079cde4a3d7e7a0688dfa4d0c6d528bd.jpg",
         description: "Primera Coya, Esposa de Manco Capac"
     },
     {
         name: "⚡ CAPAC YUPANQUI ⚡",
         rarity: "ultra rara",
         probability: 0.4,
-        img: "https://files.catbox.moe/example37.png",
+        img: "https://www.biografiasyvidas.com/biografia/t/fotos/tupac_yupanki.jpg",
         description: "Quinto Inca, Conquistador del Norte"
     },
     {
         name: "🏔️ MAYTA CAPAC 🏔️",
         rarity: "ultra rara",
         probability: 0.4,
-        img: "https://files.catbox.moe/example38.png",
+        img: "https://i.pinimg.com/474x/a3/b0/02/a3b0025131b7481a6c50cac9c548df3f.jpg",
         description: "Cuarto Inca, Guerrero de las Montañas"
     },
     {
         name: "⚰️ ATAHUALPA ⚰️",
         rarity: "ultra rara",
         probability: 0.4,
-        img: "https://files.catbox.moe/example39.png",
+        img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjNJZXFhKclrkIVT_wtnAV9MRqGNEveWONa4trzMzSjtSkFYQRa9wtErkVgZIL9SM0PE880m3ewtPLoH_Wd8k_XWYeQTHcCE05Y2KgwwoBW99A76ennFKTgbBef_sZSSau8vG2DHFmOZbQ/s680/vida-y-muerte-de-atahualpa-portada.jpg",
         description: "Último Inca Soberano, Víctima de Cajamarca"
     },
     {
         name: "🗿 INCA ROCA 🗿",
         rarity: "ultra rara",
         probability: 0.4,
-        img: "https://files.catbox.moe/example40.png",
+        img: "https://museochanchan.pe/wp-content/uploads/Inca-Roca-El-Soberano-Oculto-en-la-Historia-del-Peru-Antiguo-392x550.jpg",
         description: "Sexto Inca, Fundador de los Hanan Cusco"
     },
 
@@ -301,42 +301,42 @@ const personajesIncas = [
         name: "☀️🔥 INTI - DIOS SOL 🔥☀️",
         rarity: "Legendaria",
         probability: 0.167,
-        img: "https://files.catbox.moe/example41.png",
+        img: "https://www.infobae.com/new-resizer/ECoyAKAajc5MGkrJL3-jjBOM4RQ=/arc-anglerfish-arc2-prod-infobae/public/SS45JY2VNJBWZLZBP7PP4BO7CA.png",
         description: "Padre de todos los Incas, Dios supremo del Tahuantinsuyu"
     },
     {
         name: "🌙✨ MAMA QUILLA ✨🌙",
         rarity: "Legendaria",
         probability: 0.167,
-        img: "https://files.catbox.moe/example42.png",
+        img: "https://i.pinimg.com/736x/c2/60/ae/c260aea87e1ee766f898dd01a6f87228.jpg",
         description: "Diosa Luna, Esposa de Inti, Protectora de las mujeres"
     },
     {
         name: "⚡🌩️ ILLAPA - DIOS RAYO 🌩️⚡",
         rarity: "Legendaria",
         probability: 0.167,
-        img: "https://files.catbox.moe/example43.png",
+        img: "https://mitosymas.com/content/images/size/w1200/2023/05/David_gar_Illapa_inca_god_of_thunder_and_war_16.png",
         description: "Señor del Trueno y la Lluvia, Controlador del Tiempo"
     },
     {
         name: "🌍⛰️ PACHAMAMA ⛰️🌍",
         rarity: "Legendaria",
         probability: 0.167,
-        img: "https://files.catbox.moe/example44.png",
+        img: "https://media.urgente24.com/p/59683a100955a8f15c5a26b3eee53bc0/adjuntos/319/imagenes/003/090/0003090786/1200x0/smart/3-14jpg.jpg",
         description: "Madre Tierra, Diosa de la Fertilidad y la Naturaleza"
     },
     {
         name: "🌊🐍 MAMA COCHA 🐍🌊",
         rarity: "Legendaria",
         probability: 0.167,
-        img: "https://files.catbox.moe/example45.png",
+        img: "https://i.pinimg.com/736x/cf/cd/5b/cfcd5b7d63b6b768c749f7382e187344.jpg",
         description: "Diosa del Mar y los Lagos, Madre de las Aguas"
     },
     {
         name: "🔥👁️ VIRACOCHA 👁️🔥",
         rarity: "Legendaria",
         probability: 0.167,
-        img: "https://files.catbox.moe/example46.png",
+        img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhjcoRPaU_669nQ9Of8nQXTux7WcfqAV7Gr4Hx6iPZMf4W0s53l70Ujp6yCuouLer334RDIiOQgcPiY9mPJHDAUL69ns-oq-Qr9ZPNx05-mtidLb3jBLrcOpnYe8563qdPcI5VKbOKAygjj/s1600/viracocha-god-inca-mythology-500x696.jpg",
         description: "Creador del Universo, Dios Supremo Anterior a los Incas"
     }
 ];
@@ -384,11 +384,12 @@ let handler = async (m, { conn }) => {
 
     
     const rarityColors = {
-        'común': '🤎', // Marrón como la tierra
-        'rara': '🔵', // Azul como el cielo
-        'épica': '🟣', // Púrpura real
-        'ultra rara': '🟡', // Oro como el Sol
-        'Legendaria': '🔴' // Rojo como el fuego sagrado
+         'comun': '🌾',
+         'poco comun': '🪶', 
+         'raro': '⛰️',       
+         'epico': '🗿',       
+         'legendario': '🌞',  
+         'mitico': '👑' 
     };
 
     const rarityProbs = {
@@ -471,4 +472,5 @@ handler.group = true
 handler.cooldown = 900000
 
 export default handler
+
 
