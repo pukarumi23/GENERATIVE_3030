8a8ff388a88d22e20c975998e08e60606d1929b0
pukarumi23 <edwinsantacruzh@gmail.com>
2025-09-17 18:30:42 -0500

    Update tools-cal.js

--- plugins/tools-cal.js
+++ plugins/tools-cal.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, text }) => {
   if (id in conn.math) {
     clearTimeout(conn.math[id][3])
     delete conn.math[id]
-    m.reply('🎵✨ Cancelando cálculo musical... ✨🎵 ')
+    m.reply('🔶 Cancelando cálculo ... 🌱')
   }
   let val = text
     .replace(/[^0-9\-\/+*×÷πEe()piPI/]/g, '')
@@ -24,7 +24,7 @@ let handler = async (m, { conn, text }) => {
     console.log(val)
     let result = (new Function('return ' + val))()
     if (!result) throw result
-    m.reply(`🎤💙 *${format}* = _${result}_ 🎵✨`)
+    m.reply(`🌿 *${format}* = _${result}_ 🌱`)
   } catch (e) {
     if (e == undefined) return m.reply(`🎤💙 ¡Ingresa la ecuación musical que quieres calcular! 💙🎤\n\n✨ Símbolos compatibles: -, +, *, /, ×, ÷, π, e, (, ) ✨`)
     return m.reply(`🎵💫 Formato incorrecto en tu partitura matemática 💫🎵\n\n🎶 Solo puedes usar números 0-9 y los símbolos: -, +, *, /, ×, ÷, π, e, (, ) 🎶`)
