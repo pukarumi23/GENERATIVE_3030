fb2235cbdc112ff214e3a950c1486b3b51103a65
pukarumi23 <edwinsantacruzh@gmail.com>
2025-09-20 12:53:50 -0500

    🔶AURALIS_BOT🌿

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -3,12 +3,12 @@ import fetch from 'node-fetch'
 
 export async function before(m, { conn, participants, groupMetadata }) {
   if (!m.messageStubType || !m.isGroup) return !0;
-  const fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net"}  
+  const fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN[...]` } } }
   let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://files.catbox.moe/wm4w1x.jpg')
   let img = await (await fetch(`${pp}`)).buffer()
   let chat = global.db.data.chats[m.chat]
-  let txt = '💙 ¡Nueva Estrella Virtual! 💙'
-  let txt1 = '🎵 ¡Sayonara! 🎵'
+  let txt = '🌟 ¡Nuevo miembro se ha unido! 🌟'
+  let txt1 = '👋 ¡Hasta pronto! 👋'
   let groupSize = participants.length
   if (m.messageStubType == 27) {
     groupSize++;
@@ -17,11 +17,12 @@ export async function before(m, { conn, participants, groupMetadata }) {
   }
 
   if (chat.welcome && m.messageStubType == 27) {
-    let bienvenida = `🎤*¡Konnichiwa!* Bienvenido al concierto virtual de ${groupMetadata.subject}🎤\n\n💙 @${m.messageStubParameters[0].split`@`[0]} 💙\n\n🎵 ${global.welcom1} 🎵\n\n🌟 ¡Ahora somos ${groupSize} fanáticos de Miku! 🌟\n\n🎶 ¡Prepárate para cantar con nosotros! (◕‿◕)♡ 🎶\n\n> 🎤 Usa *#help* para ver todos los comandos mágicos de Miku! ✨\n SIGUE NUESTRO CANAL \n https://www.whatsapp.com/channel/0029VajYamSIHphMAl3ABi1o\n> 𝙏𝙀𝙏𝙊 𝙂𝙊𝙍𝘿𝘼`    
+    let bienvenida = `👋 *¡Bienvenido/a!* Te damos la bienvenida al grupo *${groupMetadata.subject}*.\n\n🌟 @${m.messageStubParameters[0].split`@`[0]} 🌟\n\nEsperamos que disfrutes tu estadía y participes con todos nosotros.\n\nActualmente somos ${groupSize} miembros.`
     await conn.sendMini(m.chat, txt, dev, bienvenida, img, img, redes, fkontak, m, rcanal)
   }
   
   if (chat.welcome && (m.messageStubType == 28 || m.messageStubType == 32)) {
-    let bye = `🎵 *¡Sayonara!* ${groupMetadata.subject} te extrañará 🎵\n\n💙 @${m.messageStubParameters[0].split`@`[0]} 💙\n\n🎤 ${global.welcom2} 🎤\n\n🌟 Ahora somos ${groupSize} fanáticos esperándote 🌟\n\n🎶 ¡Esperamos verte pronto en nuestro próximo concierto! (｡◕‿◕｡) 🎶\n\n> 🎵 ¡La música de Miku siempre te acompañará! ✨\n SIGUE NUESTRO CANAL \n https://www.whatsapp.com/channel/0029VajYamSIHphMAl3ABi1o\n> 𝙏𝙀𝙏𝙊 𝙂𝙊𝙍𝘿𝘼`
+    let bye = `👋 *¡Hasta luego!* El grupo *${groupMetadata.subject}* te extrañará.\n\n🌟 @${m.messageStubParameters[0].split`@`[0]} 🌟\n\nEsperamos verte de vuelta algún día.\n\nAhora somos ${groupSize} miembros.`
     await conn.sendMini(m.chat, txt1, dev, bye, img, img, redes, fkontak, m, rcanal)
-  }}
+  }
+}
