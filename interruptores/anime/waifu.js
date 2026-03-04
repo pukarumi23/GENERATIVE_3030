0a6624fcf2da525eefe362785a2631cbf1a746e3
chaski_3030 <202041615+pukarumi23@users.noreply.github.com>
2026-03-04 02:49:27 -0500

    🧡CHASKI🔶

--- interruptores/anime/waifu.js
+++ interruptores/anime/waifu.js
@@ -5,18 +5,18 @@ export default {
   category: 'anime',
   run: async (client, m, args, usedPrefix, command, text) => {
     try {
-      await m.react('🎤')
+      await m.react('⏱️')
       let mode = db.data.chats[m.chat]?.nsfw ? 'nsfw' : 'sfw'
       let res = await fetch(`https://api.waifu.pics/${mode}/${command}`)
       if (!res.ok) return
       let json = await res.json()
       if (!json.url) return
       let img = Buffer.from(await (await fetch(json.url)).arrayBuffer())
-      await client.sendFile(m.chat, img, 'thumbnail.jpg', `🎤 Aquí tienes tu *${command.toUpperCase()}* ฅ^•ﻌ•^ฅ`, m, global.miku)
+      await client.sendFile(m.chat, img, 'thumbnail.jpg', `🌺 Aquí tienes tu *${command.toUpperCase()}* ฅ^•ﻌ•^ฅ`, m, global.miku)
       await m.react('✔️')
     } catch (e) {
       await m.react('✖️')
-      await m.reply(`🎤 An unexpected error occurred while executing command *${usedPrefix + command}*. Please try again or contact support if the issue persists.\n> 💔 [Error: *${e.message}*]`, m, global.miku)
+      await m.reply(`🌸 An unexpected error occurred while executing command *${usedPrefix + command}*. Please try again or contact support if the issue persists.\n> 💔 [Error: *${e.message}*]`, m, global.miku)
     }
   },
-}
+} 
